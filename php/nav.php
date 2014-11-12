f24e79372db029151f66fc9fd519eefdd5d9181e
schwag <justin@myriadcore.com>
2014-11-12 15:52:13 -0500

    Header Changes

--- php/nav.php
+++ php/nav.php
@@ -2,10 +2,14 @@
                 <section class="logoBackground navbar-fixed-top">
                     <div class="container logoSection">
                         <header class="row">
-                            <div class="col-lg-6 col-sm-5">
-
+                            <div class="col-lg-2 col-sm-2">
+                                <a href="#"><img src="img/afusa_logo_250x250.png" alt"" width="90px" height="90px" class="img-responsive"></a>
                             </div>
-                            <div class="col-lg-6 col-sm-7 headercontactInfo">
+                            <div class="col-lg-6 col-sm-6">
+                                <p class="headerText">Advanced Finishing USA - 7401 Kiler Dr. Fairview, PA 16415</br>
+                                Call us today for a fast, no obligation quote.</p>
+                            </div>
+                            <div class="col-lg-4 col-sm-4 headercontactInfo">
                                 <ul class="contactNumbers">
                                   <li class="list-group-item"><a href="tel:1-814-474-5200" class="btn btn-block btn-Call" role="button">814.474.5200</a></li>
                                 </ul>
