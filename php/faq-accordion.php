67745187ac4d8219f7e982c9e9031fb6590f6cbb
schwag <justin@myriadcore.com>
2014-11-19 13:22:25 -0500

    Finished FAQS Page

--- php/faq-accordion.php
+++ php/faq-accordion.php
@@ -24,7 +24,7 @@
     </div>
     <div id="collapseTwo" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>The life of the coating depends on the application. For corrosion and abrasion coatings, two months may suffice, however 2-3 years is not a-typical. For outdoor applications, five to seven years with minimal color and gloss reduction is usual, however for patio furniture, rust may occur at welds within 1-2 years. For electrical applications such as bus bars, coatings may last as long as 10-15 years. In general, the life expectancy of powder coating is longer than paint, making it a more reliable finish.</p>
       </div>
     </div>
   </div>
@@ -38,7 +38,7 @@
     </div>
     <div id="collapseThree" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>Average cold application to a flat plate would be 1-3mils or .001" to .003". Application to a warm part can result in 3-5 mils. Spray or fluid bed dip on a hot part can achieve 8-12 mils, and multiple coats, depending on the powder resin, can yield a thickness in excess of 125 mils or 1/8 inch.</p>
       </div>
     </div>
   </div>
@@ -52,7 +52,11 @@
     </div>
     <div id="collapseFour" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>All powder-coating applications begin with the fluidization of the powder. Finely ground plastic powder is placed in a reservoir that is attached to an air plenum. The plenum and reservoir are separated by a porous membrane, which allows only air to pass through it. When clean, compressed air enters the plenum, it moves through the membrane and aerates the powder, giving it a fluidized quality. In this fluidized state, the powder may be pumped like a liquid to a spray gun. There are two primary methods of powder coating: Electrostatic Spray and Dipping.</p>
+          <p><b>Electrostatic Spray: </b>The most common method of powder application is electrostatic spray. The powder is pumped, using compressed air, from a fluidized reservoir through a spray gun. At the tip of the gun is a wire from an electrical power source, usually high voltage with micro amperage. The electricity from the wire creates a "corona" field area around the tip of the gun. When the powder passes through the corona field, the powder particles pick up an electrical charge. When these particles come in contact with a grounded metal part, they are electrostatically attracted to that part. The powder sticks to the part until the part can be placed in an oven where the powder melts and cures to the part.</p>
+          <p><a href="https://www.youtube.com/watch?v=66GBenWs0c4" target="_blank"><b>Watch our video</b></a> on electro-static powder coating.</p>
+          <p><b>Fluid Bed Dipping </b>Another method of application is called "fluid bed dipping". The part is heated to a temperature above the melting point of the plastic. The part is then dipped into the fluidized reservoir and the powder in contact with the part melts onto it. When the proper amount of powder has melted on to the part, the part is removed, any excess powder is blown off, and the part is placed in the oven for final curing.</p>
+          <p><a href="https://www.youtube.com/watch?v=1S3ylApAFSE" target="_blank"><b>Watch our video</b></a> on Fluid Bed Dipping application of powder-coat.</p>
       </div>
     </div>
   </div>
@@ -66,7 +70,7 @@
     </div>
     <div id="collapseFive" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>As with paint, there are an infinite number of colors and textures available, many of which are stock. Two common color standards that many suppliers use for color specification are RAL colors and Federal Standard colors. Unlike paint, powder cannot be made a pound at a time, so color matching is often more costly. Fortunately, there are many different powder suppliers, and they each have their own array of colors and textures. The RAL Color Chart is available below for your convenience.</p>
       </div>
     </div>
   </div>
@@ -80,7 +84,8 @@
     </div>
     <div id="collapseSix" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>There are two main types of powder coatings: Thermoplastic and thermoset. Thermoplastic powders flow when they are heated, but do not fully cure, or crosslink. After they cool, if they are re-heated, they will re-soften. They never completely set up. Thermoset powders cure when they are heated, and their resins crosslink, finishing a chemical process that cannot be reversed.</p>
+          <p>The basic powders are epoxy, urethane, nylon, vinyl, and floropolymers. Each of these resins has its inherent strengths and weaknesses, depending on the application.</p>
       </div>
     </div>
   </div>
@@ -94,7 +99,7 @@
     </div>
     <div id="collapseSeven" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>Henry Ford would have had a hard time as a powder coater. There are probably over 50 different ways to make something black. There are at least 20 different shades of black, and each powder supplier has several. There is flat black, low gloss black, varying percentages of gloss black, full gloss black, black texture, black wrinkle, black hammertone, etc. There is also black epoxy, black polyester, black nylon, black hybrid, etc. So if you just want it black, you may still have to answer a few questions.</p>
       </div>
     </div>
   </div>
@@ -108,7 +113,7 @@
     </div>
     <div id="collapseEight" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>Yes. If a customer has a paint chip, a powder supplier can match it, both color and gloss. It may not be an exact match, but it will be very close. Also, colors approaching red, orange, and yellow are more expensive, and you may have to fulfill a minimum order quantity. So if you want to coat a doorknob to match the electric yellow paint on your house, you may end up buying enough powder to coat the entire floor of the house, for the cost of a used sedan.</p>
       </div>
     </div>
   </div>
@@ -122,7 +127,7 @@
     </div>
     <div id="collapseNine" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>Our normal turnaround is 4 to 8 working days. That means if we receive your part on Monday the 1st, we will usually ship it no later than Thursday the 11th. Most parts ship within one week, and repetitive jobs have scheduled production days.</p>
       </div>
     </div>
   </div>
@@ -136,7 +141,7 @@
     </div>
     <div id="collapseTen" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>Currently, our batch capacity is 40ft long, 8ft wide, and 10ft tall. Our line capacity is 25ft long, 3ft wide, and 8ft high.</p>
       </div>
     </div>
   </div>
@@ -150,7 +155,7 @@
     </div>
     <div id="collapseEleven" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>For smaller jobs, we use cardboard, foam and shrink-wrap for packaging. For repeat jobs, we recommend reusable containers. Jobs are usually quoted with the customer responsible for all packaging supplies.</p>
       </div>
     </div>
   </div>
@@ -164,7 +169,7 @@
     </div>
     <div id="collapseTwelve" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>Incoming parts are briefly inspected for damage. In process, parts are checked for cleanliness, proper masking, thickness, cure, and adhesion. Records are kept for wash line titration levels, thickness, cure, and adhesion tests. When required, holiday spark testing can be done to 20,000 volts, cure temperature recordings can be made, and process and material certifications can be sent with finished products.</p>
       </div>
     </div>
   </div>
@@ -178,7 +183,7 @@
     </div>
     <div id="collapseThirteen" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>As with all manufacturers, environmental concerns are paramount. One of the reasons that the powder coating industry is growing is the fact that most powders contain no VOC’s. They are non-flammable, non-toxic, non-hazardous inert plastic powders. There are no regulatory constraints involved with the powders directly. As far as wastewater is concerned, all of our process and non-contact wastewater is contained and evaporated on site, with no discharge to city sewers. Our biggest environmental challenge is disposal of used powder. Although we reuse as much powder as possible, we still dispose of several hundred pounds per month of used powder that is cured into solid blocks and disposed as non-hazardous industrial waste. We are currently exploring uses for these solid plastic blocks for use on landscaping or construction.</p>
       </div>
     </div>
   </div>
@@ -192,7 +197,7 @@
     </div>
     <div id="collapseFourteen" class="panel-collapse collapse">
       <div class="panel-body">
-          <p>Test</p>
+          <p>Even though the material and process cost of powder coating are considerably higher than they are for paint, powder remains competitive. Government and environmental regulations have greatly affected the handling and disposal of paint and paint bi-products. No longer can paintbrushes, filters, and other contaminated materials be sent off to the landfills. Spent solvents that use to be used as weed killer must now be sent with manifest to disposal sites and reclaim facilities.</p>
       </div>
     </div>
   </div>
